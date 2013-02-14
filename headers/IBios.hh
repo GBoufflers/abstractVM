96cae3037df3ea6127249060427cbe61fde12c5c
ghais <ghais@localhost.localdomain>
2013-02-14 09:42:11 +0100

    dsfds

---
+++ headers/IBios.hh
@@ -0,0 +1,17 @@
+#ifndef __IBIOS_HH__
+#define __IBIOS_HH__
+
+class		IBios
+{
+public:
+  virtual ~IBios();
+  
+  virtual IOperand *createInt32(const std::string & value) = 0;
+  virtual IOperand *createInt16(const std::string & value) = 0;
+  virtual IOperand *createInt8(const std::string & value) = 0;
+  virtual IOperand *createFloat(const std::string & value) = 0;
+  virtual IOperand *createDouble(const std::string & value) = 0;
+  virtual IOperand *createOperand(eOperandType type, const std::string & value) = 0;
+}
+
+#endif
