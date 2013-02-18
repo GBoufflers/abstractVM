ae8b5b2542d79103b70afec281a7e16a3ed5b47e
ghais <ghais@localhost.localdomain>
2013-02-18 16:51:36 +0100

    yep

--- headers/IIo.hh
+++ headers/IIo.hh
@@ -3,14 +3,16 @@
 
 #include	<iostream>
 #include	<fstream>
+#include	<list>
 
 class		IIo
 {
 public:
   /*  virtual void	dump() const = 0 ;
       virtual void	print() const = 0;*/
-  virtual char	*readInput(char *file) = 0;
-  
+  virtual void	readInput(char *file) = 0;
+  virtual void  putFileInList(std::ifstream &file) = 0;
+  virtual void  putInputInList() = 0;
   virtual ~IIo(){};
 };
 
