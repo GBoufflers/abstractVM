86da4db40670c5a4b72dd98c082f7453e4abd3de
ghais <ghais@localhost.localdomain>
2013-02-23 14:43:32 +0100

    fsd

--- headers/myException.hh
+++ headers/myException.hh
@@ -10,22 +10,31 @@
 class myException : public std::exception
 {
   std::string msg;
+  const char *_Msg;
+  int	*_line;
+  std::list<std::string> *_instr;
+
 public:
-  myException(const char * Msg, int line = 0, std::list<std::string> *instr = NULL)
+  myException(const char *Msg, int line = 0, std::list<std::string> *instr = NULL) :_Msg(Msg), _line(&line), _instr(instr)
   {
-    if (instr)
-      {
-	for (std::list<std::string>::const_iterator it = instr->begin(); it != instr->end(); ++it)
-	    std::cout << *it << std::endl;
-      }
-    std::ostringstream oss;
-    if (line != 0)
-      oss <<  Msg << " à la ligne " << line << std::endl;
-    else
-      oss << Msg << std::endl;
-    this->msg = oss.str();
+    aff();
   }
   
+  virtual void aff()  throw()
+  {
+     if (_instr)
+      {
+	for (std::list<std::string>::const_iterator it = _instr->begin(); it != _instr->end(); ++it)
+	  std::cout << *it << std::endl;
+      }
+    std::ostringstream oss;
+    if (_line != 0)
+      oss <<  _Msg << " à la ligne " << _line << std::endl;
+    else
+      oss << _Msg << std::endl;
+    this->msg = oss.str();
+  }
+
   virtual ~myException() throw()
   {
     
